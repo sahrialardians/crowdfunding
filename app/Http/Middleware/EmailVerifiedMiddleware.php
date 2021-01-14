d2e384651afc7fa894285a7835babd58d5f42da1
sahrialardians <sahrialardians@gmail.com>
2021-01-14 11:27:42 +0700

    create middleware Email Verified

---
+++ app/Http/Middleware/EmailVerifiedMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class EmailVerifiedMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = auth()->user();
+
+        if($user->email_verified_at != NULL){
+            return $next($request);
+        }
+
+        return response()->json([
+            'message'=>'Email anda belum terverifikasi.',
+        ]);
+    }
+}
